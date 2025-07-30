583afacf1001586e00b9a91ea98352a871ccdb1b
Krishan Mohan <krishanmohan3501@gmail.com>
2025-07-30 22:30:40 +0530

    chnages

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middlewares/errorHandler");
 
-const express = require('express');
-const dotenv = require('dotenv').config(); 
-const cors = require('cors'); 
-const connectDB = require('./config/db'); 
-const { errorHandler } = require('./middlewares/errorHandler'); 
-
-const authRoutes = require('./routes/authRoutes');
-const patientRoutes = require('./routes/patientRoutes');
-const doctorRoutes = require('./routes/doctorRoutes');
-const appointmentRoutes = require('./routes/appointmentRoutes');
+const authRoutes = require("./routes/authRoutes");
+const patientRoutes = require("./routes/patientRoutes");
+const doctorRoutes = require("./routes/doctorRoutes");
+const appointmentRoutes = require("./routes/appointmentRoutes");
 
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin : 'https://hospital-management-system-i9m8.vercel.app',
-    credentials : true
-}));
+app.get("/", (req, res) => {
+    res.send("Backend is working!");
+});
 
 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: false })); 
-app.use('/api/auth', authRoutes);
-app.use('/api/patients', patientRoutes);
-app.use('/api/doctors', doctorRoutes);
-app.use('/api/appointments', appointmentRoutes);
+app.use(
+  cors({
+    origin: "https://hospital-management-system-i9m8.vercel.app",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/auth", authRoutes);
+app.use("/api/patients", patientRoutes);
+app.use("/api/doctors", doctorRoutes);
+app.use("/api/appointments", appointmentRoutes);
 app.use(errorHandler);
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
