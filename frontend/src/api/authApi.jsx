ef751415e903e497b422ea3e4e411b65f7675564
Krishan Mohan <krishanmohan3501@gmail.com>
2025-07-30 22:26:49 +0530

    updates

--- frontend/src/api/authApi.jsx
+++ frontend/src/api/authApi.jsx
@@ -1,17 +1,16 @@
 // frontend/src/api/authApi.js
-import axios from 'axios';
+import axios from "axios";
 
 const API_URL = import.meta.env.VITE_BACKEND_URL;
- 
+
 export const registerUser = async (userData) => {
-    return await axios.post(`${API_URL}/api/auth/register`, userData, {
-        withCredentials: true
-    });
+  return await axios.post(`${API_URL}/api/auth/register`, userData, {
+    withCredentials: true,
+  });
 };
 
 export const loginUser = async (credentials) => {
-    return await axios.post(`${API_URL}/api/auth/login`, credentials, {
-        withCredentials: true
-    });
+  return await axios.post(`${API_URL}/api/auth/login`, credentials, {
+    withCredentials: true,
+  });
 };
-
